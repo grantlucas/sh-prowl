783b7ea380457eda69c4bd8d86827114604c94d9
Grant Lucas <grantwnlucas@gmail.com>
2011-10-21 18:25:14 -0400

    Added basic implementation of prowl using Curl. Pass in the options for subject and application and you're good to go. Have a few todo's to accomplish still

---
+++ prowl.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#set the API key from the environment variable
+if [ ! -z $PROWL_APIKEY ]; then
+  API_KEY=$PROWL_APIKEY
+else
+  echo "Prowl API Key not set as an environment variable. Add \"export PROWL_APIKEY={key}\" to your .bash_profile or .profile"
+  exit 1
+fi
+
+# process options
+while getopts s:a: o
+do  case "$o" in
+  s) SUBJECT=$OPTARG;;
+  a) APPLICATION=$OPTARG;;
+  esac
+done
+# shift the option values out
+shift $(($OPTIND - 1))
+
+#TODO: Throw errors for missing options as they are all required
+
+#use everything but the options as the message to send
+MESSAGE=$*
+
+call=`curl -s -d "apikey=$API_KEY&application=\"$APPLICATION\"&event=\"$SUBJECT\"&description=\"$MESSAGE\"" https://api.prowlapp.com/publicapi/add`
+
+echo $call
+
+#TODO: parse the result with xmllint to analyze the response
