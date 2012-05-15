a3f9099ef5dd280030f9c525bc67b8c86246af8c
Grant Lucas <grantwnlucas@gmail.com>
2012-05-14 20:10:03 -0400

    Have the three main pieces of data checked for if they exist and it doesn't proceed if they don't exist

--- prowl.sh
+++ prowl.sh
@@ -8,24 +8,50 @@ else
   exit 1
 fi
 
+#Set defaults
+verbose=0
+
 # process options
-while getopts s:a: o
+while getopts s:a:v o
 do  case "$o" in
   s) SUBJECT=$OPTARG;;
   a) APPLICATION=$OPTARG;;
+  v) verbose=1;;
   esac
 done
 # shift the option values out
 shift $(($OPTIND - 1))
 
-#TODO: Throw errors for missing options as they are all required
-
 #use everything but the options as the message to send
 MESSAGE=$*
 
-call=`curl -s -d "apikey=$API_KEY&application=\"$APPLICATION\"&event=\"$SUBJECT\"&description=\"$MESSAGE\"" https://api.prowlapp.com/publicapi/add`
+#Ensure subject is supplied as it's required
+if [ -z "$SUBJECT" ]; then
+  echo "Subject is required. Use \"-s\" to set it."
+  #TODO: Create function for showing usage text
+  exit 1
+fi
 
-echo $call
+#Ensure app is supplied as it's required
+if [ -z "$APPLICATION" ]; then
+  echo "Application is required. Use \"-a\" to set it."
+  #TODO: Create function for showing usage text
+  exit 1
+fi
+
+#Ensure that a message was provided after argument parsing
+if [ -z "$MESSAGE" ]; then
+  echo "No message was provided to send."
+  exit 1
+fi
+
+
+echo $SUBJECT
+echo $APPLICATION
+echo $MESSAGE
+#call=`curl -s -d "apikey=$API_KEY&application=\"$APPLICATION\"&event=\"$SUBJECT\"&description=\"$MESSAGE\"" https://api.prowlapp.com/publicapi/add`
+
+#echo $call
 
 #TODO: Add an option to supress the use of xmllint incase it's not on a system. people can turn it off
 #TODO: parse the result with xmllint to analyze the response
